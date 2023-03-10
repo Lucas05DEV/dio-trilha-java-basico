1434de66c87b90ba18847fd7f5f4bc3074626ae6
Lucas <lucasmduarte03@gmail.com>
2023-03-10 18:46:50 -0300

    Aprendendo operadores em java.

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,30 @@
+public class Operadores {
+  public static void main(String[] args) {
+    /*
+      + positvo
+      - negativo
+      ++ incrementa
+      -- decrementa
+      ! negação
+      
+      soma +
+      subtração -
+      multiplicação *
+      divisão /
+      modulo %
+      
+      concatenação +
+
+      boolean true or false
+      && E
+      || OU
+
+      maior que >
+      menor que <
+      maior ou igual >=
+      menor ou igual <=
+      igualdade ==
+      diferença !=
+    */
+  }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //variáveis
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 10;
+        numero = 11;
+
+        System.out.println(numero);
+
+        //constante
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
     }
 }
